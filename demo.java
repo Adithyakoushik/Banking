8cd4d1c4229c5780077e78bc896ec4e272ce0c39
Adithyakoushik <adityaravi1234@gmail.com>
2023-09-08 13:17:38 +0530

    Feature1: Changed greeting message

--- demo.java
+++ demo.java
@@ -1,6 +1,1 @@
-// Demo.java
-public class Demo {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
+public class Demo { public static void main(String[] args) { System.out.println("Hello Feature1"); } }
