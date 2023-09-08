7d3264a37f0caf16205299d89766d9c8b317294e
Adithyakoushik <adityaravi1234@gmail.com>
2023-09-08 13:30:51 +0530

    Merged Feature2 into Feature1 with conflict resolution

--- demo.java
+++ demo.java
@@ -1,1 +1,1 @@
-public class Demo { public static void main(String[] args) { System.out.println("Hello Feature1"); } }
+public class Demo { public static void main(String[] args) { System.out.println("Hello Feature2"); } }
